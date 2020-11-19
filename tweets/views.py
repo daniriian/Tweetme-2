2a20d3863f7387a34cc3324b2661d2c500af0b47
daniriian <daniriian@gmail.com>
2020-11-19 09:45:13 +0200

    safe redirects

--- tweets/views.py
+++ tweets/views.py
@@ -1,20 +1,30 @@
 import random
+from django.conf import settings
 from django.http import HttpResponse, Http404, JsonResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.utils.http import is_safe_url
 
 from .forms import TweetForm
 from .models import Tweet
 
 # Create your views here.
+ALLOWED_HOSTS = settings.ALLOWED_HOSTS
+
+
 def home_view(request, *args, **kwargs):
     return render(request, "pages/home.html", context={}, status=200)
 
+
 def tweet_create_view(request, *args, **kwargs):
     form = TweetForm(request.POST or None)
+    next_url = request.POST.get('next') or None
+    print(next_url)
     if form.is_valid():
         obj = form.save(commit=False)
         # do other form related logic
         obj.save()
+        if (next_url != None and is_safe_url(next_url, ALLOWED_HOSTS)):
+            return redirect(next_url)
         form = TweetForm()
     return render(request, 'components/form.html', context={"form": form})
 
@@ -26,7 +36,8 @@ def tweet_list_view(request, *args, **kwargs):
     return json data
     """
     qs = Tweet.objects.all()
-    tweets_list = [{"id": x.id, "content": x.content, "likes": random.randint(0, 122)} for x in qs]
+    tweets_list = [{"id": x.id, "content": x.content,
+                    "likes": random.randint(0, 122)} for x in qs]
     data = {
         "isUser": False,
         "response": tweets_list
@@ -50,4 +61,5 @@ def tweet_detail_view(request, tweet_id, *args, **kwargs):
     except:
         data['message'] = "Not found"
         status = 404
-    return JsonResponse(data, status=status) # json.dumps content_type='application/json'
+    # json.dumps content_type='application/json'
+    return JsonResponse(data, status=status)
