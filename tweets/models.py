449aeda0f4595d36b5c3c726728eb63c4df402da
Daniel Tamas <daniriian@gmail.com>
2020-11-19 23:34:55 +0200

    ordering tweet list newest first

--- tweets/models.py
+++ tweets/models.py
@@ -8,9 +8,12 @@ class Tweet(models.Model):
     content = models.TextField(blank=True, null=True)
     image = models.FileField(upload_to='images/', blank=True, null=True)
 
+    class Meta:
+        ordering = ['-id']
+
     def serialize(self):
         return {
             "id": self.id,
             "content": self.content,
-            "likes": random.rand(0, 200)
+            "likes": random.randrange(200),
         }
