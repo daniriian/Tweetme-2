849a75d3568c791dc8bf94c98f4ac70f1913e59d
daniriian <daniriian@gmail.com>
2020-11-20 10:30:58 +0200

    added user to tweet, redirect to login if not authenticated

--- tweets/views.py
+++ tweets/views.py
@@ -16,12 +16,23 @@ def home_view(request, *args, **kwargs):
 
 
 def tweet_create_view(request, *args, **kwargs):
+    '''
+    REST API Create View - Django Rest Framework
+    '''
+
+    user = request.user
+    if not request.user.is_authenticated:
+        user = None
+        if request.is_ajax():
+            return JsonResponse({}, status=401)
+        return redirect(settings.LOGIN_URL)
 
     form = TweetForm(request.POST or None)
     next_url = request.POST.get('next') or None
     if form.is_valid():
         obj = form.save(commit=False)
         # do other form related logic
+        obj.user = user
         obj.save()
         if (request.is_ajax()):
             return JsonResponse(obj.serialize(), status=201)  # created items
